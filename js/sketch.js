2678e2c2f5b098563f0706d372b8e021751909a9
BlakeBrooker <116875628+BlakeBrooker@users.noreply.github.com>
2022-11-15 13:32:10 +0000

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -2,10 +2,31 @@ function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
 }
+var firstName = 'blake';
+
+var thisIsAnInterger = 100;
+
+var thisIsABoolean = true:
+
+var thisIsAnUndefinedVar;
+
+var thisIsNull= null;
+
+var ellipsex = 400;
+
+var ellipseY = 200;
+
+function setup() {
+var canvas = createCanvas (800, 800);
+canvas.parent ("p5container");
+background (220);
+console. log (thisIsAnInterger);
+}
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+fil1(255, 255, 255);
+ellipse (mouseX, mouseY, 100);
+
+console. log (mouseX, mouseY)
 }
